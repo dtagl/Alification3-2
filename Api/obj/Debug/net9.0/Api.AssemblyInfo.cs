bb88ecf49552983dbafac6623656a58573a1fb1a
dentag <dentag.com@gmail.com>
2025-11-09 14:10:32 +0500

    token_changed

--- Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf468c1a7708b61d4166f6f97be83803c5a4d10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d2de6c972f8e3c1ad9c955a83b1cf8cc580381")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
