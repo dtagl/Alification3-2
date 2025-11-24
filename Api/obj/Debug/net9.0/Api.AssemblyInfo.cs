1a00762ea94428f2bf69df23eb7831eae60f5592
dentag <dentag.com@gmail.com>
2025-11-24 12:22:40 +0500

    merging

--- Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3453cbd5e691a2e9b8dbd5a2fcf9a066d8c3ee7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777939793f0f7ebb67ce2f6f965982d40d02597f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
